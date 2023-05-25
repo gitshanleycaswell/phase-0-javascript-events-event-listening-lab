c2d420d1a9eb65fba0d00323c408165928707993
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-25 11:30:49 -0400

    adding click function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button')
+
+function clickAlert(){
+    alert('it\'s me, hi, I\'m the problem it\'s me')
 }
+
+input.addEventListener('click', clickAlert)
+}
+addingEventListener()
